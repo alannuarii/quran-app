99e17eda160dc2b4dde39f8ed51472057c871ce9
alannuarii <alannuariii@gmail.com>
2025-02-12 06:59:28 +0800

    Update

--- src/lib/components/modals/SaveReading.svelte
+++ src/lib/components/modals/SaveReading.svelte
@@ -1,5 +1,6 @@
 <script>
 	import surah from '../../quran/surah.json';
+	import quran from '../../quran/quran.json';
 
 	export let juzId;
 	export let suraId;
@@ -8,6 +9,35 @@
 	export let startData;
 
 	const allSurah = surah.data;
+	const alQuran = quran.data;
+
+	// Pastikan ayatRange selalu update ketika numberPage berubah
+	$: ayatRange = alQuran.filter((item) => item.page_number === numberPage);
+	$: arrayAyatRange = ayatRange.map((item) => item.aya_number);
+
+	// Pastikan firstAyat dan lastAyat tetap aman meskipun ayatRange kosong
+	$: firstAyat = ayatRange.length ? ayatRange[0].aya_number : '-';
+	$: lastAyat = ayatRange.length ? ayatRange[ayatRange.length - 1].aya_number : '-';
+
+	let ayatInput = '';
+	let inputClass = '';
+
+	function validateAyat(event) {
+		const value = event.target.value.trim();
+
+		if (value === '') {
+			// Jika kosong, tidak menampilkan error
+			inputClass = '';
+		} else {
+			const numericValue = parseInt(value, 10);
+			// Validasi angka apakah ada di arrayAyatRange
+			if (arrayAyatRange.includes(numericValue)) {
+				inputClass = 'is-valid';
+			} else {
+				inputClass = 'is-invalid';
+			}
+		}
+	}
 </script>
 
 <div
@@ -29,7 +59,6 @@
 						<input
 							type="number"
 							class="form-control"
-							id="floatingID"
 							placeholder="ID Plan"
 							name="end_juz"
 							value={juzId}
@@ -42,7 +71,6 @@
 						<input
 							type="text"
 							class="form-control"
-							id="floatingID"
 							placeholder="ID Plan"
 							value={allSurah[suraId - 1].surat_name}
 							readonly
@@ -53,7 +81,6 @@
 						<input
 							type="number"
 							class="form-control"
-							id="floatingID"
 							placeholder="ID Plan"
 							name="end_page_number"
 							value={numberPage}
@@ -64,13 +91,18 @@
 					<div class="form-floating mb-3">
 						<input
 							type="number"
-							class="form-control"
-							id="floatingID"
-							placeholder="ID Plan"
+							class={'form-control ' + inputClass}
+							placeholder="Ayat"
 							name="end_ayat"
+							bind:value={ayatInput}
+							on:input={validateAyat}
 							required
 						/>
-						<label for="floatingInput">Ayat</label>
+						<label for="floatingAyat">Ayat</label>
+						<p class="ms-2 mt-2">Ayat {firstAyat} - {lastAyat}</p>
+						{#if inputClass === 'is-invalid'}
+							<div id="validationServer03Feedback" class="invalid-feedback">Ayat tidak sesuai</div>
+						{/if}
 					</div>
 				</div>
 				<div class="modal-footer">
@@ -85,3 +117,11 @@
 		</div>
 	</div>
 </div>
+
+<style>
+	p {
+		margin: 0px;
+		font-style: italic;
+		font-size: 0.9rem;
+	}
+</style>
