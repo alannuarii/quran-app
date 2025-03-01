52d330fc2dfd513913fa78afad38b986e93dcbf8
alannuarii <alannuariii@gmail.com>
2025-03-01 15:50:18 +0800

    Update

--- src/routes/api/auth/callback/+server.js
+++ src/routes/api/auth/callback/+server.js
@@ -77,10 +77,3 @@ export async function GET({ url, cookies }) {
         headers: { Location: "/" },
     });
 }
-
-function generateUserId() {
-    // ID with 120 bits of entropy, or about the same as UUID v4.
-    const bytes = crypto.getRandomValues(new Uint8Array(15));
-    const id = encodeBase32LowerCase(bytes);
-    return id;
-}
